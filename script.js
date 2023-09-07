d2e38c5d6ba0a401cc214705602880574bd3eacd
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-09-07 20:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let i=0;
+	let j=1;
+	while (j<str.length()) {
+		if(str.charAt(i)!=str.charAt(j))
+			return str.charAt(i);
+		else
+			i=j;
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
